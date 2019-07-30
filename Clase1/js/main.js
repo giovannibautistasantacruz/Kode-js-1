274ccdbd17cbba2dd3aa6cbab1abe5c890573b79
Giovanni <giovanni.101793@gmail.com>
2019-07-30 17:29:22 -0500

    excersice one

---
+++ Clase1/js/main.js
@@ -0,0 +1,10 @@
+console.log((25*12)*(30));
+
+//una usb de 4 gb cuantas fotos de 3 mb le caben
+
+let usb = 4;
+let mb = mb * 1000;
+
+let maxPhoto = mb / 3;
+
+console.log(maxPhoto);
--- Class/2019-07-30/js/main.js
+++ Class/2019-07-30/js/main.js
@@ -1,1 +1,39 @@
-console.log('Hola mundo from file');
+console.log((25*12)*(30));
+
+//una usb de 4 gb cuantas fotos de 3 mb le caben
+
+var usb = 4;
+var mbSize = 4 * 1024;
+
+var maxPhoto = mbSize / 3;
+
+console.log(maxPhoto);
+
+/*
+tenemos 22 cubiculos en un estacionamiento, en cada cubicolo podemos 
+meter 3 carros y 2 camionetas .
+- Cuantos cubiculos llenamos completos?
+- Cuantas camionetas sobran?
+- Cuantos carros sobran?
+
+camionetas - 57
+carros - 130
+
+*/
+var parkingSpace = 22
+
+var van = 57;
+
+var car = 130;
+
+var totalParkingVan = parkingSpace*2; 
+var totalParkingCarr = parkingSpace *3 ;
+
+console.log(
+    'FullHouse ?',((totalParkingCarr<car) && (totalParkingVan < van)
+    )
+);
+
+
+console.log(car - totalParkingCarr)
+console.log(van - totalParkingVan)
